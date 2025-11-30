0760d370292e04bfe966257eb088a7f67013d8b5
MuhammadBurhanArshad <burhantheschoolboy@gmail.com>
2025-11-30 13:11:56 +0500

    initial commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Rest API Call')),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          final name =
+              '${user['name']['title']} ${user['name']['first']} ${user['name']['last']}';
+          final imageUrl = user['picture']['thumbnail'];
+          final email = user['email'];
+          return ListTile(
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.network(imageUrl),
+            ),
+            title: Text(name),
+            subtitle: Text(email),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: fetchUsers),
+    );
+  }
+
+  void fetchUsers() async {
+    print("Fetching Users");
+    const url = 'https://randomuser.me/api/?results=1000';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    print("fetched users completed");
+  }
+}
